d59f1790dd0851872d548f941154792adba78561
Sangwon <dnjs2618@naver.com>
2024-10-24 02:33:00 +0900

    ADD

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
-import type { Metadata } from 'next';
+import { Metadata } from 'next';
 import './globals.css';
 import Header from '../components/Header';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
-  title: '광고 없는 앱 100개 만들기 | 위드아날로그',
-  description: '더 나은 디지털 경험을 위한 광고 없는 앱 100개 만들기 프로젝트',
+  title: '위드아날로그',
+  description: '광고 없는 앱 100개 만들기 프로젝트',
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
@@ -23,6 +27,7 @@ export default function RootLayout({
       <body className="bg-gray-900 text-gray-300">
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   );
